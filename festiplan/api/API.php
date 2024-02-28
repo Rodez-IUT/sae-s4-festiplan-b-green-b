61ab632ee2504115cd9011decc2e0487a7516294
quentinformatique <quentin161204@gmail.com>
2024-02-28 17:39:10 +0100

    add send_json

--- festiplan/api/API.php
+++ festiplan/api/API.php
@@ -26,6 +26,15 @@ class API
         return $this->dataSource;
     }
 
-    
+    public function send_json($data, $status)
+    {
+        header('Access-Control-Allow-Origin: *');
+        header('Content-Type: application/json; charset=UTF-8');
+        header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT");
+
+        http_response_code($status);
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        die();
+    }
 
 }
