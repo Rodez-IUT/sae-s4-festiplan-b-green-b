9d5586fb042676e495e6a929368932dc8d68ed8b
quentinformatique <quentin161204@gmail.com>
2024-03-01 10:47:35 +0100

    Create FavorisServices.php

---
+++ festiplan/services/api/FavorisServices.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace services\api;
+
+class FavorisServices
+{
+    function getFavoris() {
+
+    }
+
+    function addFavori() {
+
+    }
+
+    function deleteFavori() {
+
+    }
+
+}
