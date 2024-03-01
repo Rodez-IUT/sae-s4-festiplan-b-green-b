24b89022eac75a0a4ae59ae1ad8565fcb47f3708
quentinformatique <quentin161204@gmail.com>
2024-03-01 16:01:04 +0100

    allFestivals et getOrganizersFestivals

--- festiplan/services/api/FestivalService.php
+++ festiplan/services/api/FestivalService.php
@@ -2,25 +2,39 @@
 
 namespace services\api;
 
+use PDOException;
+
 class FestivalService
 {
+    public static function getAllFestival($pdo): array | PDOException
+    {
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM festivals");
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            return $e;
+        }
+    }
 
-    function getAllFestival()
+    public static function getOrganizerFestival($pdo, $id): array | PDOException
+    {
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM users INNER JOIN organiser ON users.id = organiser.id_user WHERE organiser.id_festival = :id");
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            return $e;
+        }
+    }
+
+    public static function getScenesFestival()
     {
 
     }
 
-    function getOrganizerFestival()
-    {
-
-    }
-
-    function getScenesFestival()
-    {
-
-    }
-
-    function getShowsFestival()
+    public static function getShowsFestival()
     {
 
     }
