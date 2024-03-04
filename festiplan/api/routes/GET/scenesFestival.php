1a97e8021c1728855b109811ee24269ca8ebb398
quentinformatique <quentin161204@gmail.com>
2024-03-04 11:00:54 +0100

    Create scenesFestival.php

---
+++ festiplan/api/routes/GET/scenesFestival.php
@@ -0,0 +1,22 @@
+<?php
+
+use api\API;
+
+$url = explode('/', filter_var($_GET['demande'], FILTER_SANITIZE_URL));
+
+if (isset($url[1])) {
+    $result = $API->getScenesFestival($url[1]);
+    if (is_array($result)) {
+        API::send_json($result, 200);
+    } else {
+        API::send_json([
+            "status" => "KO",
+            "message" => "Erreur lors de la récupération des scènes du festival"
+        ], 500);
+    }
+} else {
+    API::send_json([
+        "status" => "KO",
+        "message" => "URL non valide, id manquant"
+    ], 400);
+}
