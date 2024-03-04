c40629be2ee2a49b957684efb1f643cf8744f19b
quentinformatique <quentin161204@gmail.com>
2024-03-04 11:01:03 +0100

    Update FestivalService.php

--- festiplan/services/api/FestivalService.php
+++ festiplan/services/api/FestivalService.php
@@ -20,7 +20,7 @@ class FestivalService
     public static function getOrganizerFestival($pdo, $id): array | PDOException
     {
         try {
-            $stmt = $pdo->prepare("SELECT * FROM users INNER JOIN organiser ON users.id = organiser.id_user WHERE organiser.id_festival = :id");
+            $stmt = $pdo->prepare("SELECT * FROM users INNER JOIN organiser ON users.idUser = organiser.idUser WHERE organiser.idFestival = :id");
             $stmt->bindParam(':id', $id);
             $stmt->execute();
             return $stmt->fetchAll();
@@ -29,13 +29,27 @@ class FestivalService
         }
     }
 
-    public static function getScenesFestival()
+    public static function getScenesFestival($pdo, $id): array | PDOException
     {
-
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM scenes INNER JOIN accueillir ON scenes.idScene = accueillir.idScene WHERE accueillir.idFestival = :id");
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            return $e;
+        }
     }
 
-    public static function getShowsFestival()
+    public static function getShowsFestival($pdo, $id): array | PDOException
     {
-
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM spectacles INNER JOIN composer ON spectacles.idSpectacle = composer.idSpectacle WHERE composer.idFestival = :id");
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            return $e;
+        }
     }
 }
