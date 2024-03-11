7bf3cafc443f4d33d6d7e3ba13ee59ac7de22a7a
quentinformatique <quentin161204@gmail.com>
2024-03-11 13:23:01 +0100

    login service

--- festiplan/services/api/LoginService.php
+++ festiplan/services/api/LoginService.php
@@ -2,10 +2,18 @@
 
 namespace services\api;
 
+use PDOException;
+use services\AuthentificationServices;
+
+
 class LoginService
 {
-    public static function login(): bool
+    public static function login($pdo, string $identifiant, string $mdp): array|bool|PDOException
     {
-        return true;
+        try {
+            return (new \services\AuthentificationServices)->is_user_valid($pdo, $identifiant, $mdp);
+        } catch (PDOException $e) {
+            return $e;
+        }
     }
 }
