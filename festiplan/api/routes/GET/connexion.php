4a8043ca40fffb1ff9cb0c130950e8b727479ed5
quentinformatique <quentin161204@gmail.com>
2024-03-11 15:17:55 +0100

    Update connexion.php

--- festiplan/api/routes/GET/connexion.php
+++ festiplan/api/routes/GET/connexion.php
@@ -8,9 +8,7 @@ $mdp = $_GET['mdp'] ?? null;
 
 if (!empty($login) && !empty($mdp)) {
     $result = $API->login($login, $mdp);
-    if ($result === false) {
-        API::send_error("Login ou mot de passe incorrect", 401);
-    } elseif ($result instanceof PDOException) {
+    if ($result instanceof PDOException) {
         API::send_error($result->getMessage(), 500);
     } else {
         API::send_json($result, 200);
