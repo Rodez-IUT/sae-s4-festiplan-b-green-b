77f9dc0ce7762393f3b5758aa48ef0a9d44a69dd
quentinformatique <quentin161204@gmail.com>
2024-03-14 11:36:43 +0100

    ajout de la suppression de favori dans l'api

--- festiplan/api/index.php
+++ festiplan/api/index.php
@@ -20,10 +20,9 @@ if (!empty($_GET["demande"])) {
             break;
         case 'DELETE':
             // si le contenu de $url[0] existe dans le dossier routes/PUT
-            if (file_exists("routes/PUT/{$url[0]}.php")) {
+            if (file_exists("routes/DELETE/{$url[0]}.php")) {
                 // on inclut le fichier correspondant
-                // TODO: mettre un clé ???
-                include_once "routes/PUT/{$url[0]}.php";
+                include_once "routes/DELETE/{$url[0]}.php";
             } else {
                 API::send_error($url[0] . " inexistant", 404);
             }
