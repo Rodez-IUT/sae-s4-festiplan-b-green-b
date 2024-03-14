0c0eb031252ff80e144051a25e8a83e21c28ddd2
quentinformatique <quentin161204@gmail.com>
2024-03-14 11:36:07 +0100

    add favoris gestion

--- festiplan/api/API.php
+++ festiplan/api/API.php
@@ -3,6 +3,7 @@
 namespace api;
 
 use PDOException;
+use services\api\FavorisService;
 use services\api\FestivalService;
 use services\api\LoginService;
 use yasmf\DataSource;
@@ -38,14 +39,14 @@ class API
     /**
      * Obtenir tous les festivals.
      *
-     * @return array|PDOException Renvoie un tableau de tous les festivals ou une PDOException s'il y a une erreur.
+     * @return string Renvoie un tableau de tous les festivals ou une PDOException s'il y a une erreur.
      */
-    public function getAllFestival(): array|PDOException
+    public function getAllFestival(): string | array
     {
         try {
             return FestivalService::getAllFestival($this->dataSource->getpdo());
         } catch (PDOException $e) {
-            return $e;
+            return $e->getMessage();
         }
     }
 
@@ -53,14 +54,14 @@ class API
      * Obtenir les festivals organisés par un organisateur spécifique.
      *
      * @param int $id L'ID de l'organisateur.
-     * @return array|PDOException Renvoie un tableau de festivals ou une PDOException s'il y a une erreur.
+     * @return string Renvoie un tableau de festivals ou une PDOException s'il y a une erreur.
      */
-    public function getOrganizerFestival($id): array|PDOException
+    public function getOrganizerFestival($id): string | array
     {
         try {
             return FestivalService::getOrganizerFestival($this->dataSource->getpdo(), $id);
         } catch (PDOException $e) {
-            return $e;
+            return $e->getMessage();
         }
     }
 
@@ -70,12 +71,12 @@ class API
      * @param int $id L'ID du festival.
      * @return array|PDOException Renvoie un tableau de scènes ou une PDOException s'il y a une erreur.
      */
-    public function getScenesFestival($id): array|PDOException
+    public function getScenesFestival($id): array | string
     {
         try {
             return FestivalService::getScenesFestival($this->dataSource->getpdo(), $id);
         } catch (PDOException $e) {
-            return $e;
+            return $e->getMessage();
         }
     }
 
@@ -85,12 +86,12 @@ class API
      * @param int $id L'ID du festival.
      * @return array|PDOException Renvoie un tableau de spectacles ou une PDOException s'il y a une erreur.
      */
-    public function getShowsFestival($id): array|PDOException
+    public function getShowsFestival($id): array | string
     {
         try {
             return FestivalService::getShowsFestival($this->dataSource->getpdo(), $id);
         } catch (PDOException $e) {
-            return $e;
+            return $e->getMessage();
         }
     }
 
@@ -101,12 +102,60 @@ class API
      * @param string $mdp
      * @return PDOException|bool|array Renvoie un tableau de favoris ou une PDOException s'il y a une erreur.
      */
-    public function login(string $login, string $mdp): PDOException|bool|array
+    public function login(string $login, string $mdp): string | bool | array
     {
         try {
             return LoginService::login($this->dataSource->getpdo(), $login, $mdp);
         } catch (PDOException $e) {
-            return $e;
+            return $e->getMessage();
+        }
+    }
+
+    /**
+     * Ajouter un favori à un utilisateur spécifique.
+     *
+     * @param int $idU L'ID de l'utilisateur.
+     * @param int $idF L'ID du festival.
+     * @param string $mdp Le mot de passe de l'utilisateur.
+     * @return string|bool Renvoie un message de confirmation ou une PDOException s'il y a une erreur.
+     */
+    public function ajouterFavori($idU, $mdp): string | bool
+    {
+        try {
+            return FavorisService::addFavori($this->dataSource->getpdo(), $idU, $mdp);
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+
+    /**
+     * Supprimer un favori.
+     *
+     * @param int $idU L'ID de l'utilisateur.
+     * @param string $mdp Le mot de passe de l'utilisateur.
+     * @return string|bool Renvoie un message de succès ou une PDOException s'il y a une erreur.
+     */
+    public function supprimerFavori($idU, $mdp): string | bool
+    {
+        try {
+            return FavorisService::deleteFavori($this->dataSource->getpdo(), $idU, $mdp);
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+
+    /**
+     * Obtenir tous les favoris d'un utilisateur spécifique.
+     *
+     * @param int $idU L'ID de l'utilisateur.
+     * @return string|array Renvoie un tableau de favoris ou une PDOException s'il y a une erreur.
+     */
+    public function getAllFavorites($idU): string | array
+    {
+        try {
+            return FavorisService::getFavoris($this->dataSource->getpdo(), $idU);
+        } catch (PDOException $e) {
+            return $e->getMessage();
         }
     }
 
@@ -139,4 +188,6 @@ class API
         echo json_encode($data, JSON_UNESCAPED_UNICODE);
         die();
     }
+
+
 }
