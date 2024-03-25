1df231c91ab7cef5ca6b67de3b7ded96613418a5
quentinformatique <quentin161204@gmail.com>
2024-03-25 15:30:47 +0100

    fix #8

--- festiplan/api/routes/GET/scenesFestival.php
+++ festiplan/api/routes/GET/scenesFestival.php
@@ -6,6 +6,9 @@ use api\API;
 if (isset($url[1])) {
     $result = $API->getScenesFestival($url[1]);
     if (is_array($result)) {
+        if (count($result) == 0) {
+            API::send_error("Festival introuvable", 404);
+        }
         API::send_json($result, 200);
     } else {
         API::send_error("Erreur lors de la récupération des scènes : " . $result, 500);
