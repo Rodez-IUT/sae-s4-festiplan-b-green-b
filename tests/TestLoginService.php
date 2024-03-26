4a95efc63386d4a4ec7ae04eab40912010f00748
quentinformatique <quentin161204@gmail.com>
2024-03-26 15:14:24 +0100

    modif test
    Co-Authored-By: François SP <75416431+Francois389@users.noreply.github.com>

--- tests/TestLoginService.php
+++ tests/TestLoginService.php
@@ -44,7 +44,7 @@ class TestLoginService extends \PHPUnit\Framework\TestCase
             // WHEN: on appelle la méthode login avec les bons identifiants
             $user = $this->loginService->login($this->pdo, 'johndoe', 'password');
 
-            // THEN: on obtient les informations de l'utilisateur
+            // THEN: on obtient les informations de l'utilisateur et sa clé API
             if ($user === false) {
                 $this->pdo->rollback();
                 $this->fail("L'utilisateur n'a pas été trouvé");
@@ -54,6 +54,7 @@ class TestLoginService extends \PHPUnit\Framework\TestCase
             $this->assertEquals('John', $user['prenomUser']);
             $this->assertEquals('johndoe', $user['loginUser']);
             $this->assertEquals($passwordHash, $user['passwordUser']);
+            $this->assertArrayHasKey('APIKey', $user);
 
             $this->pdo->rollBack();
 
@@ -92,5 +93,50 @@ class TestLoginService extends \PHPUnit\Framework\TestCase
         // THEN: on obtient une exception PDO
         $this->expectException(PDOException::class);
         $this->loginService->login($this->pdo, 'johndoe', 'passwordInconnu');
+    }
+
+    public function testAPIKeyGeneration()
+    {
+        try {
+            $this->pdo->beginTransaction();
+
+            // GIVEN: un utilisateur sans clé API
+            $idUser = $this->outilsTests->insertUser('Doe', 'John', 'test@mail.fr', 'johndoe', password_hash("password", PASSWORD_BCRYPT));
+
+            // WHEN: on appelle la méthode get_API_key
+            $key = $this->loginService->get_API_key($this->pdo, $idUser);
+
+            // THEN: on obtient une clé API
+            $this->assertNotEmpty($key);
+
+            $this->pdo->rollBack();
+
+        } catch (PDOException $e) {
+            $this->pdo->rollBack();
+            $this->fail($e->getMessage());
+        }
+    }
+
+    public function testExistingAPIKey()
+    {
+        try {
+            $this->pdo->beginTransaction();
+
+            // GIVEN: un utilisateur avec une clé API existante
+            $idUser = $this->outilsTests->insertUser('Doe', 'John', 'test@mail.fr', 'johndoe', password_hash("password", PASSWORD_BCRYPT));
+            $existingKey = $this->loginService->get_API_key($this->pdo, $idUser);
+
+            // WHEN: on appelle la méthode get_API_key
+            $key = $this->loginService->get_API_key($this->pdo, $idUser);
+
+            // THEN: on obtient la même clé API
+            $this->assertEquals($existingKey, $key);
+
+            $this->pdo->rollBack();
+
+        } catch (PDOException $e) {
+            $this->pdo->rollBack();
+            $this->fail($e->getMessage());
+        }
     }
 }
