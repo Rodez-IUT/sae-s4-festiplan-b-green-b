d213fc1191a022aa2df0b3c0a3fa2c153909b605
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-03-27 15:37:45 +0100

    Changement texte erreur

--- festiplan/api/routes/GET/tousLesFavoris.php
+++ festiplan/api/routes/GET/tousLesFavoris.php
@@ -7,8 +7,9 @@ if (isset($url[1]) && is_numeric($url[1])) {
     $result = $API->getAllFavorites($url[1]);
     if (is_array($result)) {
         if (count($result) == 0) {
-            API::send_error("Utilisateur introuvable", 400);
+            API::send_error("Aucun favoris lié à cet utilisateur", 400);
         }
+
         API::send_json($result, 200);
     } else {
         API::send_error("Erreur lors de la récupération des favoris : " . $result, 500);
